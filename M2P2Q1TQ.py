b0028eca1c79dc1fab0976a99c36b80f0fa30694
Tquran <87511438+Tquran@users.noreply.github.com>
2021-07-28 17:30:34 -0500

    Deleted
    uploaded the wrong file for this weeks assignment

--- M2P2Q1TQ.py
+++
@@ -1,7 +0,0 @@
-# Written by Tamer Quran
-# 07/15/2021
-# This Program asks for the Users name and then greets them by saying "hello _insert name_"
-name = str(input("What is your name?: "))
-print("Hello", name)
-# program works but need to remember that if it sounds simple it is not
-# at first I tried to just do "what is your name" but it took me some time to realise I needed str(input())
