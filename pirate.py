836260153b17faee3b0a18d878f7519d6f9738dc
Tquran <87511438+Tquran@users.noreply.github.com>
2021-07-28 17:32:23 -0500

    Module 3 Debugging
    These are the completed debugging labs for module 3

---
+++ pirate.py
@@ -0,0 +1,5 @@
+greeting = str(input("Hello, possible pirate! What's the password?"))
+if greeting in "Arrr!":
+	print("Go away, pirate.")
+elif greeting in "Hello":
+	print("Greetings, hater of pirates!")
